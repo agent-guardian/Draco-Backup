beb66718aa0fd3fdfac7fd3cb8d56f5e15d495be
firnen0 <37234915+firnen0@users.noreply.github.com>
2020-01-28 16:17:16 -0500

    basic command structure done

--- src/main/java/firnen/dracoBackup/Command.java
+++ src/main/java/firnen/dracoBackup/Command.java
@@ -1,9 +1,12 @@
 package firnen.dracoBackup;
 
+import java.util.Locale;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import net.minecraft.server.command.CommandManager;
@@ -13,14 +16,28 @@ import net.minecraft.text.LiteralText;
 public class Command {
 
 	public static final Logger logger = LogManager.getLogger();
-	
+
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 		LiteralArgumentBuilder<ServerCommandSource> lab = CommandManager.literal("backup")
-				.executes((ctx) -> {
-					ctx.getSource().sendFeedback(new LiteralText("Test"), false);
-					logger.info("[" + ctx.getSource().getPlayer().getName().getString() + ": used the backup command.]");
-					return 1;
-				});
+				.requires(source -> source.hasPermissionLevel(4))
+				.then(CommandManager.argument("operation", StringArgumentType.word())
+						.suggests(CommandSuggestion.getOperationSuggestions())
+						.executes((ctx) -> {
+							switch(Main.operations.indexOf(ctx.getArgument("operation", String.class).toLowerCase(Locale.ROOT))) {
+							case 0: //make
+								ctx.getSource().sendFeedback(new LiteralText("Making Backup"),true);
+								break;
+							case 1: //list
+								ctx.getSource().sendFeedback(new LiteralText("List backups"), true);
+								break;
+							case 2: //restore
+								ctx.getSource().sendFeedback(new LiteralText("Restore a Backup"), true);
+								break;
+							case 3: //schedule
+								ctx.getSource().sendFeedback(new LiteralText("Schedule regular backups"), true);
+							}
+							return 1;
+						}));
 		dispatcher.register(lab);
 	}
 }
